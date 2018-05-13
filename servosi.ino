3f16e66a74acc71ace087f546493fffa50dd50c0
alexacorzo <39241776+alexacorzo@users.noreply.github.com>
2018-05-13 16:09:22 -0500

    Add files via upload

---
+++ servosi.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+Servo myservo;
+Servo myservo2;
+
+
+void setup(){
+  myservo.attach(8); 
+  myservo2.attach(9); 
+}
+
+
+void loop(){
+  
+
+
+  myservo.write(0);
+  myservo2.write(0);
+delay(3000);
+myservo.write (90);
+myservo2.write (90);
+delay (3000);
+myservo.write (180);
+myservo2.write (180);
+delay(5000);
+
+
+  
+}
